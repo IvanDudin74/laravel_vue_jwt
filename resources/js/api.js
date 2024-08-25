8d113ebf2a45abb1d54e305b70a4c3e72e488c83
Иван <info-memo74@mail.ru>
2024-08-25 15:34:27 +0500

    commit 2

--- resources/js/api.js
+++ resources/js/api.js
@@ -16,12 +16,7 @@ api.interceptors.request.use(config => {
 //end request
 
 //start response
-api.interceptors.response.use(config => {
-        if (localStorage.getItem('access_token')) {
-            config.headers.authorization = `Bearer ${localStorage.getItem('access_token')}`
-            return config
-        }
-    },
+api.interceptors.response.use({},
     error => {
         if (error.response.data.message === 'Token has expired') {
             return axios.post('api/auth/refresh', {}, {
